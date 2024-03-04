5df613ad0c3692d022fbbb54764e435fc1aae609
Paul Summers <psummers@stanford.edu>
2024-03-04 14:42:36 -0800

    reverting running script to actually be up to date with README

--- README.txt
+++ README.txt
@@ -17,9 +17,11 @@ Input
 This code can be in run series using "thermalPocketRunner.m" You can choose to run standard processing which produces figures in the style of figure 2 of the text using "thermalPockets.m", or run the advanced processing which produces figures like the supplemental figures using "thermalPocketsSupp.m". 
 
 Output
-Running "thermalPocketRunner.m" as configured now will produce Figure 2 in the manuscript. Uncommenting lines 9-18 and line 36 will produce figures like the supplemental figures.
+Running "thermalPocketRunner.m" as configured now will produce Figure 2 in the manuscript. Uncommenting lines 19-28 and line 47 will produce figures like the supplemental figures.
 
 Running "plot3imageFigue.m" will produce figures 3,4 from the main text, though figure 3 will be produce in 2 separate plots of 3 radar lines each. 
+
+"plotQlookMvdr.m" and "plotVolumeScattering.m" were used to generate figures for responses to reviewers not used in the manuscript, but maybe useful to the user.
 
 For both of these scripts, if you select to save figures, MATLAB will pause for you to reposition the legends manually. You can do this by click and dragging the legend with your cursor. Then press any key to continue the script. 
 
--- thermalPocketRunner.m
+++ thermalPocketRunner.m
@@ -3,23 +3,26 @@ close all
 clear
 addpath lib
 
-directoryNames = ["Data_20131126_01_029"];  % MacAyeal Fig 2
-cd radarData_mv
-files = dir;
-directoryNames = {files.name};
-directoryNames = directoryNames(~ismember(directoryNames,{'.','..','.DS_Store'}));
-directoryNames = directoryNames(~contains(directoryNames,{'_Layers'}));
-cd ..
+%% This block lets you plot data for every line you have downloaded
+% cd radarData_mv
+% files = dir;
+% directoryNames = {files.name};
+% directoryNames = directoryNames(~ismember(directoryNames,{'.','..','.DS_Store'}));
+% directoryNames = directoryNames(~contains(directoryNames,{'_Layers'}));
+% cd ..
 
-% directoryNames = ["Data_20181018_01_008"];
+%% Single by name runs, currently figure 2
+directoryNames = ["Data_20131126_01_029"];
 % All files in Supp 
+
+%% Bulk by name runs, currently configured for all files in manuscript
 % directoryNames = ["Data_20131126_01_029",... 
 %                     "Data_20131126_01_031",...
 %                     "Data_20131126_01_042",...
 %                     "Data_20131126_01_047",...
 %                     "Data_20181010_02_006",...
 %                     "Data_20141115_06_006",...
-%                     "Data_20181018_01_008",... "Data_20181018_01_026",... 
+%                     "Data_20181018_01_008",...  
 %                     "Data_20121023_04_077",...
 %                     "Data_20111014_07_022",...
 %                     "Data_20181115_01_024"];  
@@ -34,14 +37,14 @@ end
                 
 [Acc, T_s] = loadALBMAP(); %accumulation rate and surface temp [m/s] [K]
 Geo = loadGEO(); %geothermal heat flux from Shen [W/m^2]          
-for ii = 74:length(directoryNames)
+for ii = 1:length(directoryNames)
     clearvars -except ii directoryNames savefig plotFigs Geo Acc T_s manualCleanUp
     savefig = true;
-    manualCleanUp = false;
+    manualCleanUp = true;
     file = erase(directoryNames{ii}, [".mat"]);
     plotFigs = true;
-%     thermalPockets; %processing like Figure 2 of the text
-    thermalPocketsSupp; %processing like the supplemental figures
+    thermalPockets; %processing like Figure 2 of the text
+%     thermalPocketsSupp; %processing like the supplemental figures
     
 end
 
